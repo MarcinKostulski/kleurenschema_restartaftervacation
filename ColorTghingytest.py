4f110393d0a21c355302dbcf4d95ea8a15fa5e00
Kostulski Marcin <marcin.kostulski@immalle.eu>
2019-04-29 12:00:06 +0200

    todo algorythm sedmi zorking, repeating issue

--- ColorTghingytest.py
+++ ColorTghingytest.py
@@ -23,28 +23,30 @@ ns = [random.randint(0,80) for x in range(100)]
 
 for n in ns:
     color =""
+    #SHORTER ALGORITHM TODO
+    for k,v in colorScheme.items():
+        if n < k:
+            color=v
+            click.secho(str(n), fg=color)
+        else:
+            continue
 
     #UNNECESSARILY LONG TEMPORARY IFELSE SOLUTION
-    if n<10:
-        color="red"
-    elif n<20:
-        color="white"
-    elif n<30:
-        color="yellow"
-    elif n<40:
-        color="black"
-    elif n<50:
-        color="green"
-    elif n<60:
-        color="cyan"
-    elif n<70:
-        color="magenta"
-    elif n<80:
-        color="blue"
-
-    #SHORTER ALGORITHM TODO
-    # for k,v in colorScheme.items():
-        # if n < k:
-        #     color=v
+    # if n<10:
+    #     color="red"
+    # elif n<20:
+    #     color="white"
+    # elif n<30:
+    #     color="yellow"
+    # elif n<40:
+    #     color="black"
+    # elif n<50:
+    #     color="green"
+    # elif n<60:
+    #     color="cyan"
+    # elif n<70:
+    #     color="magenta"
+    # elif n<80:
+    #     color="blue"
         
-    click.secho(str(n), fg=color)
+    # click.secho(str(n), fg=color)
